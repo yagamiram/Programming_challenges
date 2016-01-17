684e611f930c0298db7336c12a6cf5a7ce32d75b
Sri Ram Kannan <sriram.ssnit@gmail.com>
2016-01-17 15:17:19 -0600

    power.py

---
+++ power.py
@@ -0,0 +1,34 @@
+'''
+Find the power m^n
+source:
+https://web.stanford.edu/class/cs97si/02-mathematics.pdf
+
+Base case:
+1.If n == 0 return 1
+2. If n == 1 return m
+
+Recursive case:
+1. If the power is even then divide by 2 and
+     call recursively
+2. Else # odd case
+    divide n-1/2 and call recursively and
+    at the end multiply m*rec_call*rec_call
+'''
+
+def power(m, n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return m
+    else:
+        # check if m is odd or even
+        if n % 2  == 0:
+            # even
+            value =  power(m, n/2)
+            return value * value
+        else:
+            # odd
+            value = power(m, (n-1)/2)
+            return m*value*value
+        
+print power(-15,6)
